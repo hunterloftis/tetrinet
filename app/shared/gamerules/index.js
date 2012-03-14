efa849b68a4f6cc81ce3376d53d9ad190ff66c29
Hunter Loftis <hunter+github@hunterloftis.com>
2012-03-14 00:10:02 -0500

    Updated test/demo

--- app/shared/gamerules/index.js
+++ app/shared/gamerules/index.js
@@ -3,7 +3,7 @@ var TetrisGame = require('./tetris_game');
 var game = new TetrisGame();
 
 game.start();
-game.players[0].add_block(3, 1, 7);
+game.players[0].add_block(0, 1, 7);
 game.test_render();
 game.players[0].drop_block();
 game.test_render();
